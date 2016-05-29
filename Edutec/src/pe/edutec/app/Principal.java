7c80ab1592e3dff9b227ebd25860435beb6c3e7c
Diego Huamán Arancibia <diegohsistemas@gmail.com>
2016-05-28 21:17:31 -0500

    apuntesproyectos

---
+++ Edutec/src/pe/edutec/app/Principal.java
@@ -0,0 +1,8 @@
+package pe.edutec.app;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- SuperTec/src/pe/supertec/eduapp/service/ImplAlumno.java
+++ SuperTec/src/pe/supertec/eduapp/service/ImplAlumno.java
@@ -2,26 +2,27 @@ package pe.supertec.eduapp.service;
 
 import java.util.List;
 import pe.supertec.eduapp.cmps.AlumnoDto;
-import pe.supertec.eduapp.cmps.IfaceAlumno;
+
 
 /**
  * @author Diego Huamaán Arancibia
  * @email diegohsistemas@gmail.com
  * @GitHub DiegoHA91
  */
-public class ImplAlumno implements IfaceAlumno{
+public class ImplAlumno  {
 
-    @Override
+    AlumnoDto alumnoDto;
+    
     public void guardar(AlumnoDto obj) {
         
     }
 
-    @Override
+    
     public AlumnoDto consultar(AlumnoDto obj) {
         return null;
     }
 
-    @Override
+    
     public List<AlumnoDto> obtenerLista() {
         return null;
     }
